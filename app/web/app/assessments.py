40a38aa8717e8dc8bdebb5e4e25f5da113354be6
petrv <petr@vrubel.online>
2025-12-16 22:54:00 +0100

    Fix faulty previous fix.

--- app/web/app/assessments.py
+++ app/web/app/assessments.py
@@ -73,6 +73,8 @@ def get_assessment_edit(assessment_id: str, request:Request, current_user: User 
 @router.get("/edit/{assessment_id}/{category_order}/{question_order}", response_class=HTMLResponse, name="app_assessment_answer_question_page")
 def get_answer_question_page(assessment_id: str, category_order: int, question_order: int,  request:Request, current_user: User = Depends(user_htmx_dep)):
 
+    from app.config import SMTP_ENABLED
+
     context = {
             "request": request,
             "title":"Assessment Details",
@@ -82,15 +84,18 @@ def get_answer_question_page(assessment_id: str, category_order: int, question_o
 
     try:
         assessment_qa: list[AssessmentQA] = service.get_all_qa(assessment_id=assessment_id, current_user=current_user)
+        assessment = service.get_assessment(assessment_id=assessment_id, current_user=current_user)
         current_question: AssessmentQA = service.filter_assessment_qa_by_category_order_and_question_id(assessment_qa=assessment_qa, category_order=category_order, question_order=question_order)
         previous_question, next_question = service.get_neighbouring_questions(assessment_qa=assessment_qa, category_order=category_order, question_order=question_order)
 
         context["assessment_qa"] = assessment_qa
+        context["assessment"] = assessment
         context["title"] = f"{assessment_qa[0].assessment_name}"
         context["current_question"] = current_question
         context["wheel"] = service.prepare_wheel_context(assessment_qa=assessment_qa)
         context["previous_question"] = previous_question
         context["next_question"] = next_question
+        context["SMTP_ENABLED"] = SMTP_ENABLED
     except:
         # NotImplemented
         raise
@@ -106,6 +111,8 @@ def get_answer_question_page(assessment_id: str, category_order: int, question_o
 @router.post("/edit/{assessment_id}/{category_order}/{question_order}", response_class=HTMLResponse)
 def post_answer_question_page(answer_data: AssessmentAnswerPost, assessment_id: str, category_order: int, question_order: int,  request:Request, current_user: User = Depends(user_htmx_dep)):
 
+    from app.config import SMTP_ENABLED
+
     context = {
             "request": request,
             "title":"Assessment Details",
@@ -116,15 +123,18 @@ def post_answer_question_page(answer_data: AssessmentAnswerPost, assessment_id: 
     try:
         service.save_answer(answer_data=answer_data, current_user=current_user)
         assessment_qa: list[AssessmentQA] = service.get_all_qa(assessment_id=assessment_id, current_user=current_user)
+        assessment = service.get_assessment(assessment_id=assessment_id, current_user=current_user)
         current_question: AssessmentQA = service.filter_assessment_qa_by_category_order_and_question_id(assessment_qa=assessment_qa, category_order=category_order, question_order=question_order)
         previous_question, next_question = service.get_neighbouring_questions(assessment_qa=assessment_qa, category_order=category_order, question_order=question_order)
 
         context["assessment_qa"] = assessment_qa
+        context["assessment"] = assessment
         context["title"] = f"{assessment_qa[0].assessment_name}"
         context["current_question"] = current_question
         context["wheel"] = service.prepare_wheel_context(assessment_qa=assessment_qa)
         context["previous_question"] = previous_question
         context["next_question"] = next_question
+        context["SMTP_ENABLED"] = SMTP_ENABLED
     except:
         # NotImplemented
         raise
